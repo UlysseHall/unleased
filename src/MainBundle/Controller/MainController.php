303c90945d1f547485876dea8d85e6b14a4613cb
UlysseHall <ulysse.hall@gmail.com>
2018-01-16 17:46:11 +0100

    Login + register by API -> OK

--- src/MainBundle/Controller/MainController.php
+++ src/MainBundle/Controller/MainController.php
@@ -4,16 +4,89 @@ namespace MainBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use FOS\RestBundle\Controller\Annotations as Rest;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Session;
 
 class MainController extends Controller
 {
     /**
      * @Rest\View()
-     * @Rest\Get("/test", name="test")
+     * @Rest\Get("/register/{email}/{username}/{password}", name="main_api_register")
      */
-    public function indexAction()
+    public function RegisterAction($email, $username, $password)
     {
-        $hey = ['oui', 'non'];
-        return $hey;
+        $request = Request::createFromGlobals();
+        $tokenGenerator = $this->container->get('fos_user.util.token_generator');
+        $mailer = $this->container->get('fos_user.mailer');
+        $session = new Session();
+
+        $userManager = $this->get('fos_user.user_manager');
+        $user = $userManager->findUserBy(['username' => $username]);
+
+        if ($user !== null)
+        {
+            return ['error' => "Pseudo déjà utilisé"];
+        }
+
+        $user = $userManager->findUserBy(['email' => $email]);
+        if ($user !== null)
+        {
+            return ['error' => "Adresse email déjà utilisé"];
+        }
+
+        $user = $userManager->createUser();
+        $user->setUsername($username);
+        $user->setEmail($email);
+        $user->setPlainPassword($password);
+        $user->setEnabled(true);
+
+        if (null === $user->getConfirmationToken()) {
+            $user->setConfirmationToken($tokenGenerator->generateToken());
+        }
+
+        $userManager->updateUser($user);
+
+        $data = ['success' => true];
+
+        return $data;
+    }
+
+    /**
+     * @Rest\View()
+     * @Rest\Get("/login/{username}/{password}", name="main_api_login")
+     */
+    public function LoginAction($username, $password)
+    {
+        $request = Request::createFromGlobals();
+
+        $user = $this->getDoctrine()
+            ->getRepository('UserBundle:User')
+            ->findOneBy(['username' => $username]);
+
+        if (!$user) {
+            $user = $this->getDoctrine()
+                ->getRepository('UserBundle:User')
+                ->findOneBy(['email' => $username]);
+            if(!$user){
+                return ['error' => 'Ce pseudo ou cette adresse mail ne correspond a aucun compte'];
+            }
+        }
+
+        $isValid = $this->get('security.password_encoder')
+            ->isPasswordValid($user, $password);
+
+        if (!$isValid) {
+            return ['error' => 'Mot de passe incorrect'];
+        }
+
+        $em = $this->getDoctrine()->getEntityManager();
+
+        $user->setApiKey(hash('sha256', uniqid('', true)));
+        $em->flush();
+
+        $data = ['success' => true];
+
+        return $data;
     }
 }
--- src/UserBundle/Entity/User.php
+++ src/UserBundle/Entity/User.php
@@ -2,20 +2,32 @@
 
 namespace UserBundle\Entity;
 
-use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Model\User as BaseUser;
 
 /**
  * User
+ *
+ * @ORM\Table(name="user")
+ * @ORM\Entity(repositoryClass="UserBundle\Repository\UserRepository")
  */
 class User extends BaseUser
 {
     /**
+     * @var int
+     *
      * @ORM\Column(name="id", type="integer")
      * @ORM\Id
      * @ORM\GeneratedValue(strategy="AUTO")
      */
     protected $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="apiKey", type="string", length=255, nullable=true)
+     */
+    private $apiKey;
 
 
     /**
@@ -26,6 +38,30 @@ class User extends BaseUser
     public function getId()
     {
         return $this->id;
+    }
+
+    /**
+     * Set apiKey
+     *
+     * @param string $apiKey
+     *
+     * @return User
+     */
+    public function setApiKey($apiKey)
+    {
+        $this->apiKey = $apiKey;
+
+        return $this;
+    }
+
+    /**
+     * Get apiKey
+     *
+     * @return string
+     */
+    public function getApiKey()
+    {
+        return $this->apiKey;
     }
 }
 
